48f2c75e2b5e7f393724293bf5d31c6d84d8a6c6
axteel <stalnoyandrey.main@gmail.com>
2021-04-10 00:32:52 +0600

    additions

---
+++ src/main/java/org/lessons/builder/Customer.java
@@ -0,0 +1,7 @@
+package org.lessons.builder;/**
+* [Do not forget to leave useful description]
+* <p>
+* @author axteel on 09.04.2021 : 23:38
+* @version 1.0
+*/public class Customer {
+}
---
+++ src/main/java/org/lessons/prototype/homework/Main.java
@@ -0,0 +1,7 @@
+package org.lessons.prototype.homework;/**
+* [Do not forget to leave useful description]
+* <p>
+* @author axteel on 10.04.2021 : 00:25
+* @version 1.0
+*/public class Main {
+}
